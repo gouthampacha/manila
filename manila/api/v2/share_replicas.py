c70061a316fe582b689a4ecba744aa5cf28783af
Kiran Pawar <kinpaa@gmail.com>
2025-06-09 15:51:19 +0000

    Use encryption key ref during share create
    - Share type extra-spec "encryption_support" should be set to
     "share_server".
    - Use encryption_key_ref from user provided share create API option
    - Create ACL for barbican user from current user context, so barbican
    user can fetch the secret from secret_ref in backend driver.
    - Create server_encryption_keys project level quota which helps to
    control the encrypted share servers. Such share servers do not support
    migration/unmanage etc.
    - Create application credentials for barbican user during share server
    creation and delete during share server deletion.
    
    partially-implements: blueprint share-encryption
    Change-Id: I9e3ff98e12027eaf75cdc5f6089a016c102315d3
    Signed-off-by: Kiran Pawar <kinpaa@gmail.com>

--- manila/api/v2/share_replicas.py
+++ manila/api/v2/share_replicas.py
@@ -176,6 +176,15 @@ class ShareReplicationController(wsgi.Controller, wsgi.AdminActionsMixin):
                     "since it has been soft deleted.") % share_id
             raise exc.HTTPForbidden(explanation=msg)
 
+        if share_ref.get('instance'):
+            share_instance_id = share_ref.instance.get('id')
+            if share_instance_id:
+                instance = db.share_instance_get(context, share_instance_id)
+                if instance.get('encryption_key_ref'):
+                    msg = _("Replica cannot be created for share '%s' "
+                            "since it is encrypted.") % share_id
+                    raise exc.HTTPForbidden(explanation=msg)
+
         share_network_id = body.get('share_replica').get('share_network_id')
         if share_network_id:
             if req.api_version_request < api_version.APIVersionRequest("2.72"):
